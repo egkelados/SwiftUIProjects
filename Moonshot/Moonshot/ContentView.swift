ce76d8f05aa06b6fd63b743036872e2323142f02
Christos Mantsos <xristosman18@gmail.com>
2024-09-17 11:01:10 +0300

    add image with modifiers

---
+++ Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Xristos Mantsos on 17/9/24.
+//
+
+// TODO: Create a custom modifier for the image...?
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image(.mood)
+            .resizable()
+            .scaledToFit()
+            .containerRelativeFrame(.horizontal) {
+                size, axis in // axis = _
+                size * 0.9
+            }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Xristos Mantsos on 17/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
