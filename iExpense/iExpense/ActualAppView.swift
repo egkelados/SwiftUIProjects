e357490b3e12bdb01dacf7b478db108606180903
Christos Mantsos <xristosman18@gmail.com>
2024-09-17 10:12:27 +0300

    complete logic | add more TODOS

--- iExpense/iExpense/ActualAppView.swift
+++ iExpense/iExpense/ActualAppView.swift
@@ -1,15 +1,22 @@
-//
-//  ActualAppView.swift
-//  iExpense
-//
-//  Created by Xristos Mantsos on 16/9/24.
-//
+// MARK: Save and load Data in a clean way:
 
+/// 1. The Codable protocol, which will allow us to archive all the existing expense items ready to be stored.
+/// 2.UserDefaults, which will let us save and load that archived data.
+/// 3.A custom initializer for the Expenses class, so that when we make an instance of it we load any saved data from UserDefaults
+/// 4.A didSet property observer on the items property of Expenses, so that whenever an item gets added or removed we’ll write out changes.
 
 // TODO: Apply removeItems function to the List!
+// TODO: Find out where UserDefaults save Data..?
+// TODO: Add the ability to edit existing expense items; Add an edit feature so users can modify the name, type, or amount of an existing expense item by tapping on a row..?
+// TODO: Implement swipe actions for archiving expenses;Add a swipe gesture to allow users to "archive" an expense item, which could move it to another list or section (e.g., "Archived Expenses")..?
+// TODO: Group expenses by type (Personal or Business) in separate sections; Use SwiftUI's Section view to group the expenses by their type, organizing the list more cleanly..?
+// TODO: Add a searchbar to filter expenses by name; Implement a search feature that allows users to quickly find a specific expense by name using SwiftUI’s Searchable modifier..?
+// TODO: Display a summary of total expenses at the top of the list; Add a section at the top of the list that dynamically displays the total amount of all expenses, updating automatically as new items are added or removed..?
+
 import SwiftUI
 
-struct ExpenseItem {
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
     let name: String
     let type: String
     let amount: Double
@@ -17,24 +24,55 @@ struct ExpenseItem {
 
 @Observable
 class Expenses {
-    var items = [ExpenseItem]()
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodeItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodeItems
+                return
+            }
+        }
+        items = []
+    }
 }
 
 struct ActualAppView: View {
     @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
+    @State private var user = UserDefaults.standard
+
     var body: some View {
         NavigationStack {
             List {
-                ForEach(expenses.items, id: \.name) { item in
-                    Text(item.name)
+                ForEach(expenses.items, id: \.id) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+
+                            Text(item.type)
+                        }
+
+                        Spacer()
+
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
                 }
             }
             .navigationTitle("iExpense")
             .toolbar {
                 Button("Add Expense", systemImage: "plus") {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    expenses.items.append(expense)
+                    showingAddExpense.toggle()
                 }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
             }
         }
     }
