25eab0c8c4efbf7e7f94d9e293c5463e90306e34
Christos Mantsos <xristosman18@gmail.com>
2024-10-30 14:28:22 +0200

    add searchbar

--- Moonshot/Moonshot/Views/ActualAppView.swift
+++ Moonshot/Moonshot/Views/ActualAppView.swift
@@ -5,25 +5,40 @@
 //  Created by Xristos Mantsos on 18/9/24.
 //
 
+// TODO: Launch Date Sorting:  Add the ability to sort missions by their launch dates in the ActualAppView. This can be done with a toggle button to switch between ascending and descending order
 
-//TODO: Launch Date Sorting:  Add the ability to sort missions by their launch dates in the ActualAppView. This can be done with a toggle button to switch between ascending and descending order
-
-//TODO: Search Bar for Missions: Add a search bar to ActualAppView that filters the missions by name or crew member. This will give you experience working with search filtering in SwiftUI.
+// TODO: Search Bar for Missions: Add a search bar to ActualAppView that filters the missions by name or crew member. This will give you experience working with search filtering in SwiftUI.
 import SwiftUI
 
 struct ActualAppView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Missions] = Bundle.main.decode("missions.json")
+    @State private var search = ""
 
     let columns = [
         GridItem(.adaptive(minimum: 150))
     ]
 
+    var searchedMission: [Missions] {
+        missions.filter { mission in
+            let name = mission.displayName.lowercased().contains(search.lowercased())
+            let crew = mission.crew.contains { crewMember in
+                if let astronaut = astronauts[crewMember.name] {
+                    return
+                        astronaut.name.lowercased().contains(search.lowercased())
+                } else {
+                    return false
+                }
+            }
+            return name || crew
+        }
+    }
+
     var body: some View {
         NavigationStack {
             ScrollView {
                 LazyVGrid(columns: columns) {
-                    ForEach(missions) { mission in
+                    ForEach(search.isEmpty ? missions : searchedMission) { mission in
                         NavigationLink {
                             MissionView(mission: mission, astronauts: astronauts)
                         } label: {
@@ -52,10 +67,10 @@ struct ActualAppView: View {
                                 RoundedRectangle(cornerRadius: 10)
                                     .stroke(.lightBackground)
                             )
-                            
                         }
                     }
                 }
+                .searchable(text: $search)
                 .padding([.horizontal, .bottom])
             }
             .navigationTitle("MoonShot")
