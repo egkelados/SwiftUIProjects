efd57f321318950975aedb497c5edc13cb4cb48f
Christos Mantsos <xristosman18@gmail.com>
2024-10-30 16:06:03 +0200

    add animation to image && more infos for astronauts

--- Moonshot/Moonshot/Views/AstronautView.swift
+++ Moonshot/Moonshot/Views/AstronautView.swift
@@ -5,13 +5,15 @@
 //  Created by Xristos Mantsos on 18/9/24.
 //
 
-//TODO: Add an animation to the astronaut's image when AstronautView appears, such as a fade or scale effect.
+// TODO: Add an animation to the astronaut's image when AstronautView appears, such as a fade or scale effect.
 
-//TODO: Add a "More Info" button below the astronaut description that opens a web page with more details about the astronaut.
+// TODO: Add a "More Info" button below the astronaut description that opens a web page with more details about the astronaut.
 import SwiftUI
 
 struct AstronautView: View {
     let astronaut: Astronaut
+    @State private var animationOpacity = 0.0
+    @State private var animationScale = 0.4
 
     var body: some View {
         ScrollView {
@@ -19,14 +21,41 @@ struct AstronautView: View {
                 Image(astronaut.id)
                     .resizable()
                     .scaledToFit()
+                    .opacity(animationOpacity)
+                    .scaleEffect(animationScale)
+                    .onAppear {
+                        withAnimation(.easeOut(duration: 1.2).delay(0.2)) {
+                            animationOpacity = 1.0
+                        }
+                        withAnimation(.easeOut(duration: 1.2).delay(0.4)) {
+                            animationScale = 1.0
+                        }
+                    }
 
                 Text(astronaut.description)
                     .padding()
+
+                Button {
+                    openMoreInfo()
+                } label: {
+                    Text("More info")
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .padding()
+                        .background(.lightBackground)
+                        .clipShape(.rect(cornerRadius: 25))
+                }
             }
         }
         .background(.darkBackground)
         .navigationTitle(astronaut.name)
         .navigationBarTitleDisplayMode(.inline)
+    }
+
+    private func openMoreInfo() {
+        if let url = URL(string: "https://en.wikipedia.org/wiki/\(astronaut.name)") {
+            UIApplication.shared.open(url)
+        }
     }
 }
 
