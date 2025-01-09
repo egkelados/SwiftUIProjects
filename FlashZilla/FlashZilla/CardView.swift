202b8d21a559a2ad7f1a65f01ceb9201d407ac62
Christos Mantsos <xristosman18@gmail.com>
2025-01-09 16:22:41 +0200

    Flashzilla: implement drag gesture with offset for cards

--- FlashZilla/FlashZilla/CardView.swift
+++ FlashZilla/FlashZilla/CardView.swift
@@ -9,14 +9,17 @@ import SwiftUI
 
 struct CardView: View {
     @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
     let card: Card
-    
+
+    var removal: (() -> Void)? = nil
+
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25)
                 .fill(.white)
                 .shadow(radius: 10)
-            
+
             VStack {
                 Text(card.prompt)
                     .font(.largeTitle)
@@ -31,6 +34,23 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(offset.width / 5.0))
+        .offset(x: offset.width * 3)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(DragGesture()
+            .onChanged { gesture in
+                offset = gesture.translation
+                print("\(offset.width)")
+            }
+            .onEnded { _ in
+                if abs(offset.width) > 100 {
+                    removal?()
+                } else {
+                    offset = .zero
+                }
+            }
+        )
+
         .onTapGesture {
             isShowingAnswer.toggle()
         }
